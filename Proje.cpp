ca8d7321ad15998b363a8e88e076ef4dbe0c2020
elap1xi <gangsangwon40@gmail.com>
2022-10-12 13:55:52 +0900

    Update Proje.cpp

--- Proje.cpp
+++ Proje.cpp
@@ -340,7 +340,35 @@ main(){
 		}
 	}
 		
-		
+	// 
+	#include <stdio.h>
+	void star(int x){
+		for(int i=1; i<=x; i++){
+			printf("*");
+		}
+		printf("\n");
+	}
+	main(){
+		int n, i;
+		scanf("%d", &n);
+		for(i=1; i<=n; i++){
+			star(n+1-i);
+		}
+	}
+	
+	//
+	#include <stdio.h>
+	main(){
+		int n, i, j;
+		scanf("%d",&n);
+		for(i=1; i<=n; i++){
+			for(j=1; j<=n+1-i; j++){
+				printf("*");
+			}
+			printf("\n");
+		}
+	}
+
 	return 0; 	//코드 종료
 }
 
